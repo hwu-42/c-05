b54a567aacae7edf541cf5592866b2cdb897948a
hwu <realwuhuidong@gmail.com>
2024-12-23 19:02:29 +0100

    v0.1

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,31 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{}
+Bureaucrat::Bureaucrat(const Bureaucrat &src)
+{}
+Bureaucrat::Bureaucrat(int grade)
+{}
+Bureaucrat::Bureaucrat(std::string name)
+{}
+Bureaucrat::Bureaucrat(std::string name, int grade)
+{}
+Bureaucrat::~Bureaucrat()
+{}
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src)
+{}
+
+int Bureaucrat::getGrade(void)
+{}
+const std::string Bureaucrat::getName(void)
+{}
+void    Bureaucrat::downGrade(void)
+{}
+void    Bureaucrat::downGrade(void)
+{}
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{}
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{}
+std::ostream &operator<<(std::ostream &o, Bureaucrat *a)
+{}
---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+
+class Bureaucrat
+{
+    private:
+        const std::string   name;
+        int                 grade;
+    
+    public:
+        Bureaucrat();
+        Bureaucrat(const Bureaucrat &src);
+        Bureaucrat(int grade);
+        Bureaucrat(std::string name);
+        Bureaucrat(std::string name, int grade);
+
+        ~Bureaucrat();
+
+        Bureaucrat &operator=(const Bureaucrat &src);
+
+        int                 getGrade(void);
+        const std::string   getName(void);
+        void                upGrade(void);
+        void                downGrade(void);
+
+        class GradeTooHighException: public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class GradeTooLowException: public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat *a);
+
